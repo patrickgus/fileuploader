eed4ae7c54ac8f750a30eedd626c4752fb836ba8
Patrick Gustafson <patrickgus@charter.net>
2019-11-27 08:46:39 -0800

    use callback prop on input

--- src/SearchBox/SearchBox.js
+++ src/SearchBox/SearchBox.js
@@ -8,7 +8,10 @@ export default class SearchBox extends React.Component {
     return (
       <div className="SearchBox">
         <FontAwesomeIcon icon={faSearch}/>
-        <input placeholder="Search term" value={this.props.searchTerm}/>
+        <input 
+          placeholder="Search term" 
+          value={this.props.searchTerm}
+          onChange={e => this.props.handleUpdate(e.target.value)}/>
       </div>   
     );
   }
