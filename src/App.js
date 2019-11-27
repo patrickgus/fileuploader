84b2147d58a015c14c48a9e5e4a2fb6233c7d4f1
Patrick Gustafson <patrickgus@charter.net>
2019-11-26 23:25:14 -0800

    add props to SearchBar and FilterableList

--- src/App.js
+++ src/App.js
@@ -15,8 +15,13 @@ export default class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <SearchBar/>
-        <FilterableList files={this.props.files} />
+        <SearchBar 
+          searchTerm={this.state.searchTerm}
+          filterOption={this.state.filterOptions} />
+        <FilterableList 
+          files={this.props.files}
+          searchTerm={this.state.searchTerm}
+          filterOption={this.state.filterOptions} />
       </div>
     );
   }
