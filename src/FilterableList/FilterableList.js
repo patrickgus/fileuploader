f14bcf5e38e482777aa207794b27326d9489336c
Patrick Gustafson <patrickgus@charter.net>
2019-11-26 23:13:01 -0800

    delete extra indent

--- src/FilterableList/FilterableList.js
+++ src/FilterableList/FilterableList.js
@@ -5,7 +5,7 @@ import ListItem from '../ListItem/ListItem';
 export default class FilterableList extends React.Component {
   render() {
     const list = this.props.files
-        .map((file, key) => <ListItem {...file} key={key} />);
+      .map((file, key) => <ListItem {...file} key={key} />);
     return (
       <div className="FilterableList">
         {list}
