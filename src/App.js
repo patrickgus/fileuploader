60fd17a98118c7c0bbd849f0da02a898d854e16e
Patrick Gustafson <patrickgus@charter.net>
2019-11-26 23:21:28 -0800

    set inital state

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import SearchBar from './SearchBar/SearchBar';
 import FilterableList from './FilterableList/FilterableList';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: '',
+      filterOption: 'All'
+    }
+  }
+
   render() {
     return (
       <div className="App">
